490e352be0a355facf485d20c5ac3bf93e0cdcda
weihao <1120575939@qq.com>
2020-07-18 21:43:59 +0800

    init project

---
+++ lib/pages/index_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class IndexPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("百姓生活+"),),
+    );
+  }
+}
+
