0cbb6df1d24b1c2216b509283fbe743f16b87ff0
weihao <1120575939@qq.com>
2020-07-22 23:22:53 +0800

    TextField

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,56 @@
 import 'package:flutter/material.dart';
-class HomePage extends StatelessWidget {
+import 'package:dio/dio.dart';
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text('首页'),),
-    );
-  }
+class HomePage extends StatefulWidget {
+  _HomePageState createState() => _HomePageState();
 }
 
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    final TextEditingController typeController = new TextEditingController();
+    final showText = "showText";
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("美好人间"),
+        ),
+        body: Container(
+          height: 1000,
+          child: Column(
+            children: <Widget>[
+              TextField(
+                controller: typeController,
+                decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(10.0),
+                    labelText: "美女类型",
+                    helperText: "请输入你喜欢的类型"),
+                autofocus: false,
+                onChanged: _onChanged,
+              ),
+              RaisedButton(
+                onPressed: () {
+                  showDialog(
+                      context: context,
+                      child: new AlertDialog(
+                        title: new Text("美女类型"),
+                        content: new Text(typeController.text),
+                      ));
+                },
+                child: Text("选择完毕"),
+              ),
+              Text(
+                showText,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+  void _onChanged(String value) {
+    print(value);
+  }
+}
